9a4f4cdaf89f672ac1863a04bd5eed6205310ac5
alek-3 <jaianism.of.grotius@hotmailco.jp>
2017-11-06 23:46:54 +0900

    19章

--- xunit.py
+++ xunit.py
@@ -1,20 +1,28 @@
 class TestCase:
     def __init__(self, name):
         self.name = name
+    def setUp(self):
+        pass
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
 class WasRun(TestCase):
-    def __init__(self, name):
-        self.wasRun = None
-        super().__init__(name)
+    def setUp(self):
+        self.wasSetUp = 1
     def testMethod(self):
         self.wasRun = 1
 
 class TestCasetest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
+
+TestCasetest("testRunning").run()
+TestCasetest("testSetUp").run()
